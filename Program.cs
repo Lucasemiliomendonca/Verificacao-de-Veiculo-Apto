7c6b0b1bdb264e580f710648cdf259980dbab36a
Lucasemiliomendonca <mendoncae.metrocasa@gmail.com>
2025-07-22 13:51:52 -0300

    Verificação de Veículo Apto

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class CarroMonitoramento
+{
+     
+    public static string Verifique (string modelo, int anoFabricacao, int anoAtual)
+    {
+      int idade = anoAtual - anoFabricacao;
+      
+      if(idade <= 10)
+      {
+        return $"{modelo}: Apto";
+      }
+      else
+      {
+        return $"{modelo}: Nao apto";
+      }
+    }
+
+    static void Main()
+    {
+        
+        string modelo = Console.ReadLine()!;
+        int anoFabricacao = int.Parse(Console.ReadLine()!);
+        int anoAtual = int.Parse(Console.ReadLine()!);
+
+        
+        string resultado = CarroMonitoramento.Verifique(modelo, anoFabricacao, anoAtual);
+
+        
+        Console.WriteLine(resultado);
+    }
+}
